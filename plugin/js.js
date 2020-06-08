b9a120b489397124e75c90bd783596f7a4616d39
jimmyyen <ichthyoid95@gmail.com>
2020-06-08 10:14:21 +0800

    完成

--- plugin/js.js
+++ plugin/js.js
@@ -23,3 +23,23 @@ let inputOption1 = new Promise((resolve) => {
       })
     }, 1000)
   });
+  
+  let inputOption3 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        '#1': '超夢',
+        '#2': '皮卡丘'
+      })
+    }, 1000)
+  });
+
+  let inputOption4 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        '#1': '憤怒鳥',
+        '#2': '柯南'
+      })
+    }, 1000)
+  });
+
+
